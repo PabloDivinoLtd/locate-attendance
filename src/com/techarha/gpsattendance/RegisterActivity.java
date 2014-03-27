8b588f78aa224846eee3e1ad763be965f44c00fe
Pritesh Panjiker <priteshpanjiker@gmail.com>
2014-03-27 18:53:36 +0530

    Created ArrayAdapters and linked with XML array strings

--- src/com/techarha/gpsattendance/RegisterActivity.java
+++ src/com/techarha/gpsattendance/RegisterActivity.java
@@ -5,42 +5,43 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class RegisterActivity extends Activity {
-
-	ArrayList<String> course = new ArrayList<String>();
-	ArrayList<String> classes = new ArrayList<String>();
-	ArrayList<String> colleges = new ArrayList<String>();
-	
-	public RegisterActivity() {
-		fetchData();
-	}
-	
-	private void fetchData() {
-		course.add("MCA");
-		course.add("MBA");
 		
-		colleges.add("IBMR");
-		colleges.add("ICS");
-		
-		classes.add("First Year");
-		classes.add("Second Year");
-		classes.add("Third Year");
-	}
+	
+	
+	Button register;
+	EditText name, rollNo, password, phoneNo;
+	Spinner collegeName, className, courseName;
+	ArrayAdapter<CharSequence> collegeAdapter, classAdapter, couserAdapter;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+		
+		collegeName = (Spinner) findViewById(R.id.collegeName);
+		className = (Spinner) findViewById(R.id.className);
+		courseName = (Spinner) findViewById(R.id.courseName);
+		setArrayAdapterWithSpinners();		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.register, menu);
-		return true;
+	private void setArrayAdapterWithSpinners() {
+		collegeAdapter = ArrayAdapter.createFromResource(this, R.array.college_names, android.R.layout.simple_spinner_dropdown_item);
+		collegeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		collegeName.setAdapter(collegeAdapter);
+		
+		classAdapter = ArrayAdapter.createFromResource(this, R.array.className, android.R.layout.simple_spinner_dropdown_item);
+		classAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		className.setAdapter(classAdapter);
+		
+		couserAdapter = ArrayAdapter.createFromResource(this, R.array.couserName, android.R.layout.simple_spinner_dropdown_item);
+		couserAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		courseName.setAdapter(couserAdapter);
 	}
-
-
 }
