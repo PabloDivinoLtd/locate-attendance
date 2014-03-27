8d11598f284e8992c095f95b064ff3a8ba35f446
Pritesh Panjiker <priteshpanjiker@gmail.com>
2014-03-27 22:23:03 +0530

    added Dashboard Activity and created icon files

---
+++ src/com/techarha/gpsattendance/DashboardActivity.java
@@ -0,0 +1,20 @@
+package com.techarha.gpsattendance;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class DashboardActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dashboard);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
+	}
+}
